05870b01ba2571513ac9c3b5fb7dbfaf64768b82
Humberto Figuera <hfiguera@gmail.com>
2025-12-03 14:23:20 -0700

    Update assets.setup command in mix.exs to use npm for asset installation, improving build process consistency.

--- mix.exs
+++ mix.exs
@@ -67,7 +67,7 @@ defmodule PipeSpot.MixProject do
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
-      "assets.setup": ["esbuild.install --if-missing"],
+      "assets.setup": ["cmd --cd assets npm install", "esbuild.install --if-missing"],
       "assets.build": ["esbuild default"],
       "assets.deploy": ["esbuild default --minify", "phx.digest"]
     ]
